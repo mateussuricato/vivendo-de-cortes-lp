5582aef16a63619ad1b854ba3221f216da015641
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2023-12-14 16:53:01 -0300

    feature: background image add

--- src/components/header/style.js
+++ src/components/header/style.js
@@ -7,10 +7,16 @@ export const Container = styled.div`
     align-items: center;
     justify-content: center;
     font-family: 'Sora', sans-serif;
+    background-image: url("/images/abstract.webp");
+    background-position: center center;
+	background-repeat: no-repeat;
+	background-size: cover;
+   
 
     .headline {
         font-size: 2rem;
-        margin-bottom: 1rem;
+        margin-top: 5rem;
+        margin-bottom: 5rem;
         max-width: 550px;
         text-align: center;
         font-weight: 400;
