8caa3a5798480f8b99a882d141038ce8b7ead86d
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2023-12-15 17:21:09 -0300

    feature: remove head

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Main from './components/Main';
 function App() {
   return (
     <div className="App">
-
         <Main />
     </div>
   );
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -5,7 +5,6 @@ export default function Main() {
         <div>
      
             <S.Container>
-
                 <S.FlexOne>
                     <h1>+300.000.000 de views</h1>
                     <p>Com <span className="negrito">1 corte viral</span> você atinge milhões de seguidores, constrói uma audiência engajada, <span className="colorido">monetiza</span> sua conta e o melhor de tudo <span className="colorido">lucra muito</span></p>
