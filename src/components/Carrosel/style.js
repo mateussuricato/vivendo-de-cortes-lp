5d03c3f6b729cac1dbe9ba678dfe51ba2356960b
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2024-06-26 22:47:47 -0300

    feature: change carrossel add mobile

--- src/components/Carrosel/style.js
+++ src/components/Carrosel/style.js
@@ -3,11 +3,16 @@ import styled from 'styled-components';
 export const Container = styled.div`
     display: flex;
     gap: 1rem;
-    overflow: hidden;
+    overflow-x: scroll; /* Permite rolagem horizontal */
+    overflow-y: hidden; /* Evita rolagem vertical */
     max-width: 1150px;
     cursor: grab;
     user-select: none; /* Evita a seleção de texto/imagens */
-    touch-action: pan-y; /* Permite apenas o scrolling vertical na página */
+    touch-action: pan-x; /* Permite apenas o scrolling horizontal na página */
+
+    .item {
+        flex: 0 0 auto; /* Evita o colapso dos itens */
+    }
 
     .item img {
         border-radius: 12px;
