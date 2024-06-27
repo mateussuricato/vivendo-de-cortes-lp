98c7dfc22ec8b7e1a9252f14f6b83c0328874eef
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2024-06-27 18:10:10 -0300

    feature: test iphone

--- src/components/Carrossel/style.js
+++ src/components/Carrossel/style.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Depoimentos = styled.div`
-    display: flex;
+     display: flex;
     margin: auto;
     max-width: 100%;
     overflow-x: auto;
@@ -9,6 +9,8 @@ export const Depoimentos = styled.div`
     margin: 3rem 0 1rem 0;
     cursor: grab;
     user-select: none;
+    touch-action: pan-y;
+    scroll-behavior: smooth;
 
     &::-webkit-scrollbar {
         display: none;
@@ -20,7 +22,9 @@ export const Depoimentos = styled.div`
         width: 250px;
         height: 100%;
         pointer-events: none;
-        user-select: none;
+        user-select: none; /* Evita a seleção de texto/imagens */
+        transition: transform 0.2s; /* Adiciona uma transição suave ao arrastar */
+        display: inline-block;
         border-radius: 12px;
         border: 2px solid transparent;
         background-image: linear-gradient(white, white), linear-gradient(45deg, #FF7F50, #FF4500, #FF8C00, #FFD700, #FFA500);
