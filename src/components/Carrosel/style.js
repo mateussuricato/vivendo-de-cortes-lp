19731a48f4e3d8fe4563b304ce68c550ebbcba12
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2024-06-26 22:54:24 -0300

    feature: change carrossel mobile

--- src/components/Carrosel/style.js
+++ src/components/Carrosel/style.js
@@ -3,20 +3,23 @@ import styled from 'styled-components';
 export const Container = styled.div`
     display: flex;
     gap: 1rem;
-    overflow-x: scroll; /* Permite rolagem horizontal */
+    overflow-x: auto; /* Permite rolagem horizontal */
     overflow-y: hidden; /* Evita rolagem vertical */
-    max-width: 1150px;
+    max-width: 100%;
     cursor: grab;
     user-select: none; /* Evita a seleção de texto/imagens */
     touch-action: pan-x; /* Permite apenas o scrolling horizontal na página */
+    scroll-behavior: smooth; /* Adiciona suavidade à rolagem */
 
     .item {
         flex: 0 0 auto; /* Evita o colapso dos itens */
+        min-width: 300px; /* Define uma largura mínima para os itens */
     }
 
     .item img {
         border-radius: 12px;
-        max-width: 300px;
+        width: 100%; /* Faz com que a imagem ocupe toda a largura do item */
+        height: auto; /* Mantém a proporção da imagem */
         pointer-events: none;
         user-select: none; /* Evita a seleção de texto/imagens */
     }
