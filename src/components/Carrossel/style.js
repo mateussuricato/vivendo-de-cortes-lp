367457c4e56853edadf38ca5a02b44e1d152e111
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2024-06-28 00:24:24 -0300

    feature: change size carrossel

--- src/components/Carrossel/style.js
+++ src/components/Carrossel/style.js
@@ -6,9 +6,9 @@ export const Container = styled.div`
 margin-top: 3rem;
 
 img {
-    width: 250px;
+    width: 300px;
     display: inline-block;
-    border: 2px solid transparent;
+    border: 3px solid transparent;
     background-image: linear-gradient(white, white), linear-gradient(45deg, #FF7F50, #FF4500, #FF8C00, #FFD700, #FFA500);
     background-origin: border-box;
     background-clip: content-box, border-box;
@@ -16,7 +16,7 @@ img {
 }
 
 .swiper {
-  width: 250px;
+  width: 300px;
 
 }
 
