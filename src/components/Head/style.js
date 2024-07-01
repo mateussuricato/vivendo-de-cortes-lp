4e68bd009582bf27448a0f110960b4e3a27b4b1d
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2024-07-01 15:11:14 -0300

    feature: remove padding headline

--- src/components/Head/style.js
+++ src/components/Head/style.js
@@ -8,7 +8,7 @@ export const Container = styled.div`
 	background-size: cover;
 
     img {
-        padding: 3rem 0 2rem 0;
+        padding: 3rem 0 1rem 0;
         display: flex;
         justify-self: center;
         align-self: center;
@@ -23,7 +23,7 @@ export const Container = styled.div`
         text-align: center;
         font-weight: 400;
         margin: 0 auto 0 auto;
-        padding-top: 2rem;
+        padding-top: 1rem;
         padding: 0 1rem;
 
         @media (max-width: 579px) {
@@ -49,7 +49,6 @@ export const Container = styled.div`
 `
 
 export const VslContainer = styled.div`
-	padding: 1rem;
     max-width: 700px;
 	margin: 1.2rem auto 0 auto;
 `
