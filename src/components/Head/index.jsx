1ea02673aeb3dfe16d484ca8328a1d15f76bc8b2
Mateus Nunes <96783028+mateussuricato@users.noreply.github.com>
2024-07-02 00:10:08 -0300

    feature: fix words head components

--- src/components/Head/index.jsx
+++ src/components/Head/index.jsx
@@ -6,7 +6,7 @@ export default function Head() {
         <S.Container>
         <img src="/images/logo.webp" alt="" />
             <div className="headline">
-                <span className="negrito">Descrubra</span> o Método para Ganhar <span className="sublinhado">de 5 a 10 mil por mês</span> <span className="colorido">com Cortes Virais,</span> Sem Aparecer, Usando Apenas o Celular.  
+                <span className="negrito">Descubra</span> o Método para Ganhar <span className="sublinhado">de 5 a 10 mil por mês</span> <span className="colorido">com Cortes Virais,</span> Sem Aparecer, Usando Apenas o Celular.  
             </div>
             <S.VslContainer>
             <Vsl />
